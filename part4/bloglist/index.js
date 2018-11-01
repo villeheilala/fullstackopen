5c48600aedff5b4f0046809f43359e5751f6710c
Ville Heilala <ville@heila.la>
2018-11-01 21:03:40 +0200

    Complete 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,45 +4,28 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const Blog = mongoose.model('Blog', {
-	title: String,
-	author: String,
-	url: String,
-	likes: Number
-})
-
-module.exports = Blog
-
-app.use(cors())
-app.use(bodyParser.json())
+const blogsRouter = require('./controllers/blogs')
 
 if ( process.env.NODE_ENV !== 'production' ) {
 	  require('dotenv').config()
 }
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose
+	.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+	.then(() => {
+		console.log('Connectedc to database ', process.env.MONGODB_URI)
+	})
+	.catch(err => {
+		console.log(err)
+	})
 
-app.get('/api/blogs', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
-})
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
 
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body)
+app.use('/api/blogs', blogsRouter)
 
-	blog
-		.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
-})
-
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
