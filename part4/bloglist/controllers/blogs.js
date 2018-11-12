bbc6c6c47d0bad23ab511ad978a7175494c674a0
Ville Heilala <ville@heila.la>
2018-11-12 09:35:45 +0200

    Complete 4.18-4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -32,15 +33,30 @@ blogsRouter.get('/:id', async (request, response) => {
 	}
 })
 
+const getTokenFrom = (request) => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		return authorization.substring(7)
+	}
+	return null
+}
+
 blogsRouter.post('/', async (request, response) => {
+	const body = request.body
+	
 	try {
-		const body = request.body
+		const token = getTokenFrom(request)
+		const decodedToken = jwt.verify(token, process.env.SECRET)
+
+		if (!token || !decodedToken.id) {
+			return response.status(401).json({ error: 'Token missing or invalid' })
+		}
 
 		if (body.title === undefined || body.author === undefined || body.url === undefined) {
 			return response.status(400).json({ error: 'Bad request: title is  missing.' })
 		}
 
-		const user = await User.findById(body.userId)
+		const user = await User.findById(decodedToken.id)
 
 		const blog = new Blog({
 			title: body.title,
@@ -58,8 +74,12 @@ blogsRouter.post('/', async (request, response) => {
 		response.status(201).json(Blog.format(savedBlog))
 
 	} catch (exception) {
-		console.log(exception)
-		response.status(500).json({ error: 'error happened...' })
+		if (exception.name === 'JsonWebTokenError') {
+			response.status(401).json({ error: exception.message })
+		} else {
+			console.log(exception)
+			response.status(500).json({ error: 'error happened...' })
+		}
 	}
 })
 
