b4f6613c3be974a1e213853a4be9fe9f13f89f9b
Ville Heilala <ville@heila.la>
2019-11-29 13:09:08 +0200

    Complete 8.14

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -18,64 +18,6 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
     console.log('error connection to MongoDB:', error.message)
   })
 
-/*
- * It would be more sensible to assosiate book and the author by saving 
- * the author id instead of the name to the book.
- * For simplicity we however save the author name.
-*/
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
-
 const typeDefs = gql`
   type Author {
     name: String!
@@ -125,6 +67,9 @@ const resolvers = {
     authorCount: () => Author.collection.countDocuments(),
     allBooks: (root, args) => {
       // filter missing
+      if (args.genre) {
+        return Book.find({ genres: { $in: [args.genre] } })
+      }
       return Book.find({})
     },
     /*
@@ -144,22 +89,22 @@ const resolvers = {
     allAuthors: () => Author.find({}),
   },
   Author: {
-    bookCount: (root) => books.filter(book => book.author === root.name).length
+    bookCount: async (root, args) => {
+      const books = await Book.countDocuments({ author: root.id })
+      return books
+    }
   },
   Mutation: {
     addBook: async (_, args) => {
       let author = await Author.findOne({ name: args.author })
-      console.log(author)
       if (!author) {
         author = new Author({ name: args.author })
         author = await author.save()
       }
       const book = new Book({ ...args, author: author.id})
-      //books = books.concat(book)
       try {
         await book.save()
       } catch (error) {
-        console.log(book)
         throw new UserInputError(error.message, {
           invalidArgs: args,
         })
@@ -167,23 +112,18 @@ const resolvers = {
       return book
     },
     editAuthor: async (_, args) => {
-      const updateAuthor = await Author.findOne({ name: args.name })
-      if (!updateAuthor) return null
-      const updatedAuthor = {
-        ...updateAuthor,
-        born: args.setBornTo
-      }
+      const author = await Author.findOne({ name: args.name })
+      if (!author) return null
+      author.born = args.setBornTo
+      console.log(args)
       try {
-        await updateAuthor.save()
+        await author.save()
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args,
         })
       }
-      //authors = authors.map(author => 
-      //  author.name === updatedAuthor.name ? updatedAuthor : author
-      //)
-      return updatedAuthor
+      return author
     }
   }
 }
